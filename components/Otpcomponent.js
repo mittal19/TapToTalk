ee5900b74d73d2da6a92f3063b836eb98bcfab47
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2020-10-30 15:53:51 +0530

    minor change in otpcomponent

--- components/Otpcomponent.js
+++ components/Otpcomponent.js
@@ -1,3 +1,5 @@
+//https://github.com/tttstudios/react-native-otp-input
+
 import React,{Component} from 'react';
 import {View,Text,TextInput,TouchableOpacity,ToastAndroid} from 'react-native';
 import OTPInputView from '@twotalltotems/react-native-otp-input';
@@ -15,18 +17,22 @@ export function Otpcomponent({route,navigation})
 
   return(
     <View>
-      <OTPInputView
-        style={{width: '80%', height: 200}}
-        pinCount={4}
-          // code={this.state.code} //You can supply this prop or not. The component will be used as a controlled / uncontrolled component respectively.
-    // onCodeChanged = {code => { this.setState({code})}}
-        autoFocusOnLoad
-        codeInputFieldStyle={{width:30,height: 45,borderWidth: 0,borderBottomWidth: 1}}
-        codeInputHighlightStyle={{borderColor: "#03DAC6"}}
-        onCodeFilled = {(code) => {
-          console.log(`Code is ${code}, you are good to go!`)
-        }}
-        />
+       <Text>Enter OTP</Text>
+        <OTPInputView
+          style={{width: '80%', height: 200}}
+          pinCount={4}
+          autoFocusOnLoad
+          keyboardType="phone-pad"
+          codeInputFieldStyle={{width:30,height: 45,borderWidth: 0,borderBottomWidth: 1,color:'#000000'}}
+          codeInputHighlightStyle={{borderColor: "#000000"}}
+          onCodeFilled = {(code) => {
+            console.log(`Code is ${code}, you are good to go!`)
+            }}
+          />
+        <TouchableOpacity ><Text>Enter</Text></TouchableOpacity>
+        <Text>OR</Text>
+        <TouchableOpacity onPress={otpnotrecieved}><Text>Didn't recieved OTP on {Phonenumber} ?</Text></TouchableOpacity>
+
     </View>
   );
 
